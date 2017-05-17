18fe222da69db9cd635f77208ab832379ea36208
Firedrake969 <firedrake969@users.noreply.github.com>
2017-05-17 17:37:35 -0600

    add files

---
+++ databatch.py
@@ -0,0 +1,64 @@
+import sys
+import numpy as np
+import random
+import math
+import json
+
+laser_only = False
+camera_only = True
+verbose_batch = False
+very_verbose_batch = False
+
+def open_file(f):
+    with open('{}.json'.format(f), 'wb') as out:
+        return json.load(out)
+
+print 'opening files'
+data = {}
+data['laser'] = open_file('dict_data/laser')
+data['ackermann'] = open_file('dict_data/ackermann')
+data['camera'] = open_file('dict_data/camera')
+print 'files opened'
+
+# let's use ackermann data as the base and fill in from there by weighted averaging of camera and laser info
+
+def get_relatived_value(arr, rel_idx):
+    forward_bias, idx = math.modf(rel_idx * len(arr))
+    if idx+1 == len(arr):
+        return arr[-1]
+    first_bias = 1 - forward_bias
+    v1 = np.array(arr[int(idx)]) * first_bias
+    v2 = np.array(arr[int(idx)+1]) * forward_bias
+    return (v1 + v2).tolist()
+
+def shuffle_data(features, targets):
+    zipped = zip(features, targets)
+    random.shuffle(zipped)
+    return zip(*zipped)
+
+max_batch_size = len(data['ackermann'].keys())
+def batch(batch_size):
+    batch_size = min(max_batch_size, batch_size)
+    targets = []
+    features = []
+    times = random.sample(data['ackermann'].keys(), batch_size)
+    for idx, time in enumerate(times):
+        item_count = len(data['ackermann'][time])
+        if time not in data['laser'] or time not in data['camera']:
+            continue
+        for second_idx, value in enumerate(data['ackermann'][time]):
+            rel_idx = float(second_idx)/item_count
+            laser_single = get_relatived_value(data['laser'][time], rel_idx)
+            targets.append([value])
+            if laser_only:
+                features.append(laser_single)
+            elif camera_only:
+                features.append(get_relatived_value(data['camera'][time], rel_idx))
+            else:
+                camera_single = get_relatived_value(data['camera'][time], rel_idx)
+                features.append(camera_single + laser_single)
+        if verbose_batch and very_verbose_batch:
+            print 'batch prep percent', float(idx)/batch_size
+    if verbose_batch:
+        'returning batch size={}'.format(batch_size)
+    return (features, targets)
---
+++ parsebag.py
@@ -0,0 +1,81 @@
+# This doesn't matter unless you have the actual .bag files, which aren't given in the git repo since each is multiple gigabytes.
+
+import sys
+import json
+import rosbag
+
+# import numpy as np
+# from cv_bridge import CvBridge
+# import cv2
+
+def save_file(name, data):
+    with open('{}.json'.format(name), 'wb') as f:
+        json.dump(data, f)
+
+def parse_ackermann(ackermann):
+    d = ackermann.drive
+    return d.steering_angle
+
+bag = rosbag.Bag(sys.argv[1])
+laser = {}
+ackermann = {}
+camera = {}
+
+TOPICS = {
+    '/sensors/scan': 'laser',
+    '/sensors/usb_cam/image_raw': 'camera',
+    '/vesc/ackermann_cmd_mux/input/teleop': 'drive',
+}
+
+# print 'reading bag'
+camera_messages = 0
+total_camera_messages = 712
+# bridge = CvBridge()
+
+# def auto_canny(image, sigma=0.33):
+#     # compute the median of the single channel pixel intensities
+#     blur_size = 3
+#     image = cv2.GaussianBlur(image, (blur_size, blur_size), 0)
+#     # v = np.median(image)
+#     v = np.mean(image)
+ 
+#     # apply automatic Canny edge detection using the computed median
+#     lower = int(max(0, (1.0 - sigma) * v))
+#     upper = int(min(255, (1.0 + sigma) * v))
+#     edged = cv2.Canny(image, lower, upper)
+ 
+#     # return the edged image
+#     return edged
+
+last_image = None
+for topic, msg, t in bag.read_messages(topics=TOPICS.keys()):
+    seconds = t.secs
+    if TOPICS[topic] == 'laser':
+        if seconds in laser:
+            laser[seconds].append(msg.ranges + msg.intensities)
+        else:
+            laser[seconds] = [msg.ranges + msg.intensities]
+    if TOPICS[topic] == 'camera':
+        camera_messages += 1
+        imgdata = bridge.imgmsg_to_cv2(msg)
+        imgdata = cv2.resize(imgdata[:,:,0], (0, 0), fx=0.25, fy=0.25)
+        if seconds not in camera:
+            camera[seconds] = [imgdata.flatten().tolist()]
+            if last_image is not None:
+                camera[seconds-1].append(last_image.flatten().tolist())
+                last_image = None
+            print float(camera_messages)/float(total_camera_messages)
+        else:
+            last_image = imgdata
+    if TOPICS[topic] == 'drive':
+        if seconds in ackermann:
+            ackermann[seconds].append(parse_ackermann(msg))
+        else:
+            ackermann[seconds] = [parse_ackermann(msg)]
+
+bag.close()
+print 'bag closed'
+
+save_file('dict_data/laser', laser)
+save_file('dict_data/ackermann', ackermann)
+save_file('dict_data/camera', camera)
